3b12d4b6393ccb476439da256961c9852c1d86a7
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-20 06:46:54 -0300

    aula 114 teoria sobre objetos

---
+++ CursoWebModernoCompletocomJavaScript2022/cap7_objeto/aula-114.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTAVEL
+
+// PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OBJETOS oo
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        
+    }
+}
+objeto.processamento()
+
+/* Pricipios imporantes: **/
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
