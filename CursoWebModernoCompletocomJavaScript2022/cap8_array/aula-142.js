cd4aec77636cc49e55a9368e2ab03512986d2d08
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-22 20:57:56 -0300

    aula-142 #reduce 2

---
+++ CursoWebModernoCompletocomJavaScript2022/cap8_array/aula-142.js
@@ -0,0 +1,20 @@
+const alunos = [
+    { nome: 'João', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: true },
+]
+
+// Desafio 1: Todos os alunos são bolsistas?
+const todosBolsistas = ( resultado, bolsista ) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista)) // retorna todas condições
+
+const todosBolsistas2 = ( resultado, bolsista ) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+// Desafio 2: ALgum aluno é bolsista?
+const todosBolsistas3 = ( resultado, bolsista ) => resultado && bolsista
+console.log(!alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+const todosBolsistas4 = ( resultado, bolsista ) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas4))
