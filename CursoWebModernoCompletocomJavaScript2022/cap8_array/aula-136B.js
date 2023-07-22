6b106672ab9df472ae61ccbd25d31c3540a1f6b8
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-22 12:44:43 -0300

    aula-137 #map2 muito top

--- CursoWebModernoCompletocomJavaScript2022/cap8_array/aula-136B.js
+++ CursoWebModernoCompletocomJavaScript2022/cap8_array/aula-136B.js
@@ -3,5 +3,4 @@ let produto1 = 199.05
 const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
 
 console.log(paraDinheiro(200.10))
-console.log('================================================================================')
 console.log(paraDinheiro(produto1))
